50e20ba1d30830542b4d8a5bdf4b387a5d547536
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-28 20:42:34 +0100

    Update build configuration

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
+
+plugins {
+  `java-library`
+  id("io.papermc.paperweight.userdev") version "1.7.1"
+  id("xyz.jpenilla.run-paper") version "2.3.0"
+  id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.1.1"
+}
+
+group = "net.slqmy.strongerfishingrods"
+version = "1.0-SNAPSHOT"
+
+val javaVersion = 21
+val paperApiVersion = "1.21"
+
+java {
+  toolchain.languageVersion = JavaLanguageVersion.of(javaVersion)
+}
+
+dependencies {
+  paperweight.paperDevBundle(paperApiVersion + "-R0.1-SNAPSHOT")
+}
+
+tasks {
+  compileJava {
+    options.release = javaVersion
+  }
+
+  javadoc {
+    options.encoding = Charsets.UTF_8.name()
+  }
+}
+
+bukkitPluginYaml {
+  main = "net.slqmy.strongerfishingrods.StrongerFishingRods"
+  load = BukkitPluginYaml.PluginLoadOrder.STARTUP
+  authors.add("Slqmy")
+  apiVersion = paperApiVersion
+}
